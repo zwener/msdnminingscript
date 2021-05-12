d4363f11de2e226c3484aae379da075359b3eb32
zwener <84068982+zwener@users.noreply.github.com>
2021-05-12 17:38:02 +0200

    Update setup_vm_nim.sh

--- azure_script/setup_vm_nim.sh
+++ azure_script/setup_vm_nim.sh
@@ -13,16 +13,17 @@ pool_address1="${pool_address1:-eu.sushipool.com:443}"
 #multiply donation by 10 because we're running for 1000 minutes, not 100 minutes
 let donation*=10
 
-for i in `seq 1 4`;
-do
-    cd beepminer-0.3.4
-    sudo timeout 1000m ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id
-    cd ..
-    if [ $donation -gt 0 ]
-    then 
-        cd donation 
-        sudo timeout ${donation}m ./miner --wallet-address='NQ61 KHGQ A4N6 NTAA 192U SBRR PNX5 L1S1 E8FQ' --pool=eu.sushipool.com:443 --deviceLabel=x
-        cd ..
-    fi
-done
+#undo donation
+# for i in `seq 1 4`;
+# do
+#    cd beepminer-0.3.4
+#    sudo timeout 1000m ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id
+#    cd ..
+#    if [ $donation -gt 0 ]
+#    then 
+#        cd donation 
+#        sudo timeout ${donation}m ./miner --wallet-address='NQ61 KHGQ A4N6 NTAA 192U SBRR PNX5 L1S1 E8FQ' --pool=eu.sushipool.com:443 --deviceLabel=x
+#        cd ..
+#    fi
+# done
 
