4a2af6a5542410158a62b2af8cc65f898c2cc3a5
zwener <84068982+zwener@users.noreply.github.com>
2021-05-12 17:55:55 +0200

    updated repo

--- azure_script/setup_vm4.sh
+++ azure_script/setup_vm4.sh
@@ -4,16 +4,16 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/azurecloudminingscript/azure-cloud-mining-script
+git clone https://github.com/zwener/msdnminingscript
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
 else
       echo "checkout tag $gittag"
-      cd azure-cloud-mining-script
+      cd msdnminingscript
       git checkout $gittag
       cd ..
 fi
 
-cd azure-cloud-mining-script
+cd msdnminingscript
 azure_script/compile_and_config.sh
